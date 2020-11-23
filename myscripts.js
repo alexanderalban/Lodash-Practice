180d537728d0bb59b89bb21f94acef928ecd96a2
alexanderalban <albanalexander@gmail.com>
2020-11-22 21:40:11 -0600

    added front-facing site

--- myscripts.js
+++ myscripts.js
@@ -52,6 +52,8 @@ const pieNums = ["1","4","1","5","9","2","6","5","3","5","8","9","7","9","3",
 "6","8","0","6","6","1","3","0","0","1","9","2","7","8","7","6","6","1","1",
 "1","9","5","9","0","9","2","1","6","4","2","0","1","9","8","9"];
 
+
+
 //_.chunk creates an array of elements split into groups. In this case,
 //splitting the pieNums into groups of 200.
 console.log("This is the '_.chunk'")
@@ -104,3 +106,92 @@ console.log(_.drop(pieNums, 500));
 //the first instance of "0" will be replaced with "hello"
 console.log("this is the '_.replace'")
 console.log(_.replace(pieNums, '0', 'hello'));
+
+
+
+//Examples for front facing website
+
+//Get Full List of Office Workers
+
+let officeArray
+let workerNameArray = [];
+
+const getOfficeArray = () => {
+    fetch('https://randomuser.me/api/?nat=us&results=50&inc=name')
+        .then(response => response.json())
+        .then(users => officeArray = users)
+        setTimeout(function() {
+            console.log(officeArray)
+            officeArray.results.map((user) => {
+                workerNameArray.push(user.name.first);
+            })
+            console.log(workerNameArray);
+        }, 1000)
+};
+
+getOfficeArray();
+
+const showWorkerNameArray = () => {
+    let office = document.getElementById('office-home');
+    let workers = workerNameArray.join(', ');
+    office.append(workers);
+}
+
+//Get Mystery Team
+
+const getMysteryTeam = () => {
+   mysteryTeam = _.sampleSize(workerNameArray, 5);
+   console.log(mysteryTeam);
+   let teamMates = document.getElementById('team-home');
+   let team = mysteryTeam.join(', ');
+   teamMates.append(team);
+}
+
+//_.chunk
+
+const divideAndConquer = () => {
+    let interrogationRooms = document.getElementById('chunk-home');
+    let suspectGroups = _.chunk(workerNameArray, [size=10]);
+    console.log(suspectGroups)
+    interrogationRooms.append(suspectGroups.join(' \b '));
+}
+
+//_.reverse
+
+const reverseWorkers = () => {
+    let room = document.getElementById('reverse-home');
+    let flippedWorkers = _.reverse(workerNameArray);
+    console.log(flippedWorkers);
+    room.append(flippedWorkers.join(', '));
+}
+
+//_.shuffle
+
+const shuffleWorkers = () => {
+    let room = document.getElementById('shuffle-home');
+    let shuffledWorkers = _.shuffle(workerNameArray);
+    console.log(shuffledWorkers);
+    room.append(shuffledWorkers.join(', '));
+}
+
+//_.without
+const removeSome = () => {
+    let room = document.getElementById('without-home');
+    let remainingWorkers =  _.without(workerNameArray, "John", "Cody", "Allison", "Eric", "Jenny",
+    "Ann", "Tina", "Jim", "Ryan", "James");
+    console.log(remainingWorkers);
+    room.append(remainingWorkers.join(', '));
+}
+
+
+//Solve Mystery
+const solveMystery = () => {
+    let room = document.getElementById("solve-home");
+    _.shuffle(workerNameArray);
+    let innocents = workerNameArray.splice(0, 49);
+    console.log(innocents);
+    console.log(workerNameArray);
+    let remainingWorkers = _.without(workerNameArray, innocents)
+    console.log(remainingWorkers);
+    room.append(remainingWorkers.join(', '));
+}
